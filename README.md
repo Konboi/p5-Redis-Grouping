2c84198ca349a36f76947863442fac4a3d906605
Konboi <ryosuke.yabuki@gmail.com>
2015-04-20 19:26:13 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Redis::Grouping - It's new $module
+
+# SYNOPSIS
+
+    use Redis::Grouping;
+
+# DESCRIPTION
+
+Redis::Grouping is ...
+
+# LICENSE
+
+Copyright (C) Konboi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Konboi <ryosuke.yabuki@gmail.com>
---
+++ lib/Redis/Grouping.pm
@@ -0,0 +1,39 @@
+package Redis::Grouping;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Redis::Grouping - It's new $module
+
+=head1 SYNOPSIS
+
+    use Redis::Grouping;
+
+=head1 DESCRIPTION
+
+Redis::Grouping is ...
+
+=head1 LICENSE
+
+Copyright (C) Konboi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Konboi E<lt>ryosuke.yabuki@gmail.comE<gt>
+
+=cut
+
